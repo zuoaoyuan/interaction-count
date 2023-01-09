be20d92ebb319ae29862242c11c8dc989c01eb2a
Aoyuan Zuo <zuoaoyuan@google.com>
2023-01-09 09:40:33 -0500

    Add Interaction Count Demo * add main page * add preview image for og * include touchstart to showcase android testing

---
+++ main.js
@@ -0,0 +1,80 @@
+const MIN_DURATION_THRESHOLD = 16;
+
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+function logEvents() {
+    let lastTenEntries = [];
+    let firstInteractionId;
+    const log = $('#event-log');
+
+    const getInteractionNumber = (entry) => {
+        // This code is an estimate until proper interactionCount is supported.
+        return Math.round((entry.interactionId - firstInteractionId) / 7) + 1;
+    };
+
+    new PerformanceObserver((list) => {
+        for (const entry of list.getEntries()) {
+            if (entry.interactionId || entry.name == 'touchstart') {
+                if (!firstInteractionId && entry.interactionId) {
+                    firstInteractionId = entry.interactionId;
+                }
+                const interactionNumber = entry.interactionId ? getInteractionNumber(entry) : 'N/A';
+                const tr = document.createElement('tr');
+                tr.innerHTML = `
+          <td>${interactionNumber}</td>
+          <td>${entry.name}</td>
+          <td class="value">${entry.duration}</td>
+          <td class="value">
+            <code>${new Date().toISOString().slice(11)}</code>
+          </td>
+        `;
+                log.prepend(tr);
+            }
+        }
+    }).observe({ type: 'event', durationThreshold: MIN_DURATION_THRESHOLD });
+}
+
+function initialize() {
+
+    const updateCountDisplay = () => {
+        const interactionCountValueElement = $('#interaction-count-value');
+        if (interactionCountValueElement.innerHTML != performance.interactionCount)
+            interactionCountValueElement.innerHTML = performance.interactionCount;
+    }
+
+    // Initialize event listener for updating interaction count.
+    ['keydown', 'keyup', 'pointerdown', 'pointerup'].forEach((type) => {
+        addEventListener(type, updateCountDisplay, { passive: true });
+    });
+
+    const block = (blockingTime = 0) => {
+        const blockingStart = performance.now();
+        while (performance.now() < blockingStart + blockingTime) {
+            // Block...
+        }
+    }
+
+    // Initialize event listener blocking time.
+    ['keydown', 'keyup', 'pointerdown', 'pointerup', 'click', 'touchstart'].forEach((type) => {
+        addEventListener(type, (event) => { block(MIN_DURATION_THRESHOLD) }, { passive: true });
+    });
+
+    logEvents();
+}
+
+if (
+    'PerformanceEventTiming' in self &&
+    'interactionId' in PerformanceEventTiming.prototype
+) {
+    initialize();
+} else {
+    document.body.classList.add('unsupported');
+    alert(
+        [
+            `Oops, this brower does not fully support the Event Timing API,`,
+            `which is required for this demo.`,
+        ].join(' ')
+    );
+}
